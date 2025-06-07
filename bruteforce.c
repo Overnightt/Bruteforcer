9627304db3e519c1bc46248ebc1b1eb183f2ca9b
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-06-07 11:33:52 +0200

    Update bruteforce.c

--- bruteforce.c
+++ bruteforce.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void bruteforce ( char pwd, char solution, char list)
+void bruteforce ( char pwd, char solution, char list, int i) {
         if (strcmp(solution, pwd) == 0)
                 printf ("%s",solution);
         else
                 for(int i=0, i<94; i++)
-
-
+                        solution=list[i];
+                        i = i+1;
 
 }
