5980193725da21e646c12dfaf028663eab6fde75
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-06-08 16:17:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,8 +10,8 @@ int main() {
         printf("Please enter a password, all characters are allowed, maximum 20 characters   ");
         scanf("%s", pwd);
         }
-        char solution[20] = ""; /*this will be the guess of the bruteforce function */
+        char solution[21]; /*this will be the guess of the bruteforce function */
         char list[] = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890&é(-è_çà)=$*ù!:;,<>#{[^@]}¤£µ%./§?°+";  /*this is the list of all possible character in the password */
-        bruteforce ( pwd, solution, list, 0, 0);
+        bruteforce ( pwd, solution, list, 0);
     return 0;
 }
