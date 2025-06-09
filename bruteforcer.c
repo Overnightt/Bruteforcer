ddcd1795e8ec4afc245546587b6816297b3f579c
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-06-09 11:06:50 +0200

    Update and rename bruteforce.c to bruteforcer.c

---
+++ bruteforcer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "bruteforcer.h"
+#include <stdlib.h>
+
+int bruteforce ( char pwd[], int pwd_max_len,char solution[], char list[], int l) {
+        printf("Trying: %s \n", solution); /* This line is optional, it is just to see what solution the algorith is trying*/
+        if (strcmp(solution, pwd) == 0) {
+                printf ("the password is: %s \n",solution);
+                exit(0);
+        }
+        if (l >= pwd_max_len) return 0;
+        else{
+                for(int i=0; i<94; i++) {
+                        solution[l] = list[i];
+                        solution[l + 1] = '\0';
+                        bruteforce(pwd,pwd_max_len,solution,list,l+1);
+
+                }
+        }
+}
