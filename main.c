491e9fb23694286957c0170c06c8ef5ce0f9c524
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-06-09 11:05:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 
 int main() {
     char pwd[100]="";
-    while ((strlen(pwd) > 20) || (strlen(pwd) == 0))
+    int pwd_max_len=10;    /* if you want to change the maximal length of a password*/
+    while ((strlen(pwd) > pwd_max_len) || (strlen(pwd) == 0))
         {
-        printf("Please enter a password, all characters are allowed, maximum 20 characters   ");
+        printf("Please enter a password, all characters are allowed, maximum %d characters ", pwd_max_len);
         scanf("%s", pwd);
         }
-        char solution[21]; /*this will be the guess of the bruteforce function */
+        char solution[100] = ""; /*this will be the guess of the bruteforce function */
         char list[] = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890&é(-è_çà)=$*ù!:;,<>#{[^@]}¤£µ%./§?°+";  /*this is the list of all possible character in the password */
-        bruteforce ( pwd, solution, list, 0);
+        bruteforce ( pwd, pwd_max_len ,solution, list, 0);
     return 0;
 }
+~ 
