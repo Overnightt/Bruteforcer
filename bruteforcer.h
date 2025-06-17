d2026f5b2997aa883a037be10930f00ebb272376
Adrien Djouder Fey <113140101+Overnightt@users.noreply.github.com>
2025-06-17 11:12:48 +0200

    Update bruteforcer.h

--- bruteforcer.h
+++ bruteforcer.h
@@ -1,6 +1,6 @@
 #ifndef BRUTEFORCER_H
 #define BRUTEFORCER_H
 
-int bruteforce(char pwd[], int pwd_max_len ,char solution[], char list[], int l);
+int bruteforce(char pwd[], int pwd_max_len ,char solution[], char list[], int l,int *found);
 
 #endif
